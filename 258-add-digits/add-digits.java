95e1e9c94bca8da3272e66e3a3b835c6bd7a7bf4
Vedant <vedantmehetre95@gmail.com>
2026-08-14 21:56:49 +0530

    Time: 1 ms (97.96%) | Memory: 42.4 MB (91.45%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -3,6 +3,9 @@ class Solution {
         if(num == 0){
             return 0;
         }
-        return 1+ (num -1) % 9;
+        if(num %  9 == 0) {
+            return 9;
+        }
+        return num % 9;
     }
 }
