31eb36f3c9efcabc77d7999c182769d3ad90258e
Vedant <vedantmehetre95@gmail.com>
2026-09-06 12:09:12 +0530

    Time: 3 ms (99.80%) | Memory: 46.3 MB (60.34%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -3,14 +3,14 @@ class Solution {
         int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] symbol = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
 
-        String result = "";
+         StringBuilder result = new StringBuilder();
 
         for(int i = 0; i < values.length;i++) {
             while(num >= values[i]) {
-                result = result + symbol[i];
-                num = num - values[i];
+                result.append(symbol[i]);
+                num -= values[i];
             }
         }
-        return result;
+        return result.toString();
     }
 }
